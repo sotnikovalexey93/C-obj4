380243d76991715224883b513bb3f5de1ec3e1af
Alexey <sotnikovalexey93@mail.ru>
2023-01-25 19:19:51 +0300

    task002

---
+++ task001/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16
+*/
+
+Console.WriteLine("Введите число А");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int Exponentiation(int numberA, int numberB)
+
+{
+  int result = 1;
+  for(int i=1; i <= numberB; i++)
+  {
+    result = result * numberA;
+  }
+    return result;
+}
+
+int exp = Exponentiation(a, b);
+Console.WriteLine("Ответ: " + exp);
---
+++ task002/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12
+*/
+
+
+Console.WriteLine("Введите число:");
+int a = Convert.ToInt32(Console.ReadLine());
+int sum(int a)
+{
+    int count = Convert.ToString(a).Length;
+    int adv = 0;
+    int result = 0;
+
+    for (int i = 0; i < count; i++)
+    {
+        adv = a - a % 10;
+        result = result + (a - adv);
+        a = a / 10;
+    } 
+    return result;
+}
+int Summ = sum(a);
+Console.WriteLine($"Сумма всех цифр числа {a}, будет {Summ}" );
